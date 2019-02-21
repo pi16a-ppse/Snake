8efa5a0b48d8c81c49dbc0a4da8243971ad4f047
Veronika <nazar2539@gmail.com>
2019-02-21 11:46:20 +0300

    Added control settings

---
+++ Setting/Settings.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeGame
+{
+	public enum Directions
+	{
+		Left,
+		Right,
+		Up,
+		Down
+	}
+
+	class Settings
+	{
+		public static int Width {get;set;}
+		public static int Height {get;set;}
+		public static int Speed {get;set;}
+		public static int Score {get;set;}
+		public static int Points {get;set;}
+		public static int Width {get;set;}
+		
+	}
+	
+	
+	
+	};
