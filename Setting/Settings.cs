fec6ca170f09efcd058fc2eab45250c16cc30a14
Veronika <nazar2539@gmail.com>
2019-03-21 07:46:36 +0300

    Addwd directions and parametrs for snake

--- Setting/Settings.cs
+++ Setting/Settings.cs
@@ -1,31 +1,22 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SnakeGame
+namespace WindowsFormsApp1
 {
-	public enum Directions
-	{
-		Left,
-		Right,
-		Up,
-		Down
-	}
-
-	class Settings
-	{
-		public static int Width {get;set;}
-		public static int Height {get;set;}
-		public static int Speed {get;set;}
-		public static int Score {get;set;}
-		public static int Points {get;set;}
-		
-		public static Directions direction {get;set;}
-		
-	}
-	
-	
-	
-	};
+    public enum Directions {        //Объявление направлений движения змейки
+        Left, Right, Up, Down
+    }
+    class Settings
+    {
+        public static int Height { get; set; }
+        public static int Widht { get; set; }
+        public static int Speed{ get; set; }
+        public static int Point { get; set; }
+        public static int Score { get; set; }
+        public static bool  EndGame { get; set; }
+        public static Directions direction { get; set; }
+    }
+}
