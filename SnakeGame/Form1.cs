4dac6b6e587cc3678efbb8a66e1ded4e182fa252
Veronika <nazar2539@gmail.com>
2019-03-21 08:01:18 +0300

    Create class with events for a snake

---
+++ SnakeGame/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void keyisdown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void keyisup(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void updateGraphics(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
