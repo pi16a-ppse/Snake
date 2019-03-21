5cfb84f799e04a537b2308680ad033b306f98798
Veronika <nazar2539@gmail.com>
2019-03-21 07:52:34 +0300

    Added default value parametrs snake

--- Setting/Settings.cs
+++ Setting/Settings.cs
@@ -13,10 +13,24 @@ namespace WindowsFormsApp1
     {
         public static int Height { get; set; }
         public static int Widht { get; set; }
-        public static int Speed{ get; set; }
+        public static int Speed { get; set; }
         public static int Point { get; set; }
         public static int Score { get; set; }
-        public static bool  EndGame { get; set; }
+        public static bool EndGame { get; set; }
         public static Directions direction { get; set; }
+
+
+        public Settings() {
+            Widht = 17;
+            Height = 17;
+            Speed = 15;
+            Score = 0;
+            Point = 70;
+            EndGame = false;
+            direction = Directions.Down;
+                 
+        }   
     }
+
+    
 }
